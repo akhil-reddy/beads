aae90fa80808fdfe2684d20c25bc946073e7a1ca
Akhil K <blitzkrieg.akhil@gmail.com>
2025-02-04 13:57:34 -0800

    Coarse Implementation | CMU | Push Implementation | Convert | Vision

--- beads/core/cmu/sequencing/convert/vision.py
+++ beads/core/cmu/sequencing/convert/vision.py
@@ -1,8 +1,26 @@
 """
     A (256, 256, 256) number is converted into a varying length vector for stimulus.
-}
 """
 
 
-def get_stimulus(red, green, blue):
+def get_stimulus_pixel(red, green, blue):
+    pass
+
+
+"""
+    An RGB image (or a frame from a video) is processed into a varying length vector matrix
+"""
+
+
+def get_stimulus_frame(image):
+    pass
+
+
+"""
+    A static video or realtime video is processed in the same style as an Event Camera 
+    (https://en.wikipedia.org/wiki/Event_camera)
+"""
+
+
+def get_stimulus_video(frames):
     pass
