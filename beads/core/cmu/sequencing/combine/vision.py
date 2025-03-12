9931019a5945fff324997ed0911335fa4472004c
Akhil K <blitzkrieg.akhil@gmail.com>
2025-03-11 19:55:13 -0700

    Fine Implementation | CMU | Push Implementation | Combine & Convert | Vision

--- beads/core/cmu/sequencing/combine/vision.py
+++ beads/core/cmu/sequencing/combine/vision.py
@@ -4,44 +4,59 @@ The combine operation of the Push Implementation (refer Turing Beads.good-notes)
 Before this operation, the RGB stimulus is converted into a lightweight "beehive" like structure. This structure
 is analogous to a set of coloured rain drops separated by a membrane.
 
-After conversion, each unit / "drop" is combined with its neighbours based on similarity. The membrane
-is dissolved, and they form a larger "drop".
-
-The functionalities of horizontal and bipolar cells are implemented here. Horizontal
-cells spread their signals laterally to inhibit nearby photoreceptors. This “lateral inhibition” improves
-contrast and sharpens edges. Essentially, before bipolar cell processing, horizontal cells makes the stimulus
-interact constructively / destructively so that "context" spreads out horizontally.
-
 """
 
-'''
-In a traditional ML sense, this operation is equivalent to channel preprocessing in a
-Convolutional Neural Network (CNN).
-
-Consider this datastructure:
-
-{
-    area: NUMBER, provides flexibility on the shape of the drop without describing the exact
-        shape, which is of secondary significance
-    *pointers: LIST of all neighbours, but carefully mapped to a "sub number" in the area.
-        This esoteric structure should be equivalent to the Turing implementation of a "drop".
-    stimulus: DATA, contains the RGB transformed structure for stimulus.
-
-REQUIREMENT: The "quality" of this operation should match or exceed one of CNN.
-'''
+"""
+After conversion, each unit / "drop" is combined with its neighbours based on similarity. The membrane
+is dissolved, and they form a larger "drop" i.e., the functionalities of horizontal cells are implemented here. 
+Horizontal cells spread their signals laterally to inhibit nearby photoreceptors. The spread is dynamic and
+influenced by neurotransmitters like dopamine. This “lateral inhibition” improves
+contrast and sharpens edges. Essentially, before bipolar cell processing, horizontal cells makes the stimulus
+interact constructively / destructively so that "context" spreads out horizontally.
+"""
 
 
-def convert():
-    pass
+class Horizontal:
+    def __init__(self):
+        pass
+
+    '''
+    In a traditional ML sense, this operation is equivalent to channel preprocessing / convolution in a
+    Convolutional Neural Network (CNN).
+    
+    Consider this datastructure:
+    
+    {
+        area: NUMBER, provides flexibility on the shape of the drop without describing the exact
+            shape, which is of secondary significance
+        *pointers: LIST of all neighbours, but carefully mapped to a "sub number" in the area.
+            This esoteric structure should be equivalent to the Turing implementation of a "drop".
+        stimulus: DATA, contains the RGB transformed structure for stimulus.
+    }
+    REQUIREMENT: The "quality" of this operation should match or exceed one of CNN.
+    '''
+
+    def link(self):
+        pass
+
+    '''
+    In a traditional ML sense, this operation is equivalent to the convolutional operator
+    in a CNN. However, the analogy is made purely for understanding purposes. 
+    
+    REQUIREMENT: The "quality" of this operation should match or exceed one of CNN.
+    '''
+
+    def inhibit(self):
+        pass
 
 
-'''
-In a traditional ML sense, this operation is equivalent to the convolutional operator
-in a CNN. However, the analogy is made purely for understanding purposes. 
+"""
+Bipolar cells further refine the laterally inhibited stimulus through ON-OFF push-pull mechanism. Furthermore,
+they prepare the graded stimulus for spike transmission by enhancing the stimulus at high details and inhibiting
+stimulus when its at low detail. Neurotransmitters are highly active here. 
+"""
 
-REQUIREMENT: The "quality" of this operation should match or exceed one of CNN.
-'''
 
-
-def combine():
-    pass
+class Bipolar:
+    def __init__(self):
+        pass
