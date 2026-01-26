92d369916c52cf0bcb15fda706dc5d16b39daa0e
Akhil K <blitzkrieg.akhil@gmail.com>
2026-01-25 19:35:02 -0800

    Fine Implementation | CMU | Components | All

--- beads/components/cmu/audio.py
+++ beads/components/cmu/audio.py
@@ -1,6 +1,31 @@
-def get_stimulus_frame(wave):
-    pass
+import sounddevice as sd
+import numpy as np
+
+from beads.core.cmu.sequencing.receive.audio import OuterEar
+
+FS = 44100
+ear_model = OuterEar(fs=FS, realtime=True)
 
 
-def get_stimulus_continuous(waves):
-    pass
+def get_stimulus_continuous(indata, frames, time, status):
+    if status:
+        print(status)
+
+    raw_audio = indata[:, 0] + 1j * indata[:, 1]
+
+    processed_pressure = ear_model.function(raw_audio, input_is_pa=False, ref_db=94.0)
+
+    peak_pa = np.max(np.abs(processed_pressure))
+    print(f"Peak Pressure: {peak_pa:.2f} Pa", end="\r")
+
+    outdata = processed_pressure.real + 1j * processed_pressure.imag
+
+
+if __name__ == "__main__":
+    try:
+        with sd.Stream(samplerate=FS, channels=2, callback=get_stimulus_continuous):
+            print("Listening...Press Ctrl+C to stop")
+            while True:
+                sd.sleep(100)
+    except KeyboardInterrupt:
+        print("Stopped listening.")
--- beads/components/cmu/vision.py
+++ beads/components/cmu/vision.py
@@ -1,17 +1,35 @@
 """
     An RGB image (or a frame from a video) is processed into a varying length vector matrix
 """
+import cv2
 
 
 def get_stimulus_frame(image):
+    # Use vision cell functions
     pass
 
 
-"""
-    A static video or realtime video is processed in the same style as an Event Camera 
-    (https://en.wikipedia.org/wiki/Event_camera)
-"""
+if __name__ == "__main__":
+    # Load vision cells
 
+    cap = cv2.VideoCapture(0)
 
-def get_stimulus_video(frames):
-    pass
+    if not cap.isOpened():
+        print("Error opening video stream or file")
+        exit()
+
+    while True:
+        ret, frame = cap.read()
+
+        if not ret:
+            print("Empty camera frame.")
+            break
+
+        get_stimulus_frame(frame)
+        # cv2.imshow('frame', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
